6dc9144f5853a1254cf1d9eba3319862158b58f1
Pierre Garric <pierregarric1@gmail.com>
2017-10-01 00:45:05 +0200

    Update DM.py

--- DM.py
+++ DM.py
@@ -1,15 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt #importation des bibliothèques nécessaires au graphes
-from math import sqrt
+from math import sqrt #et ici de la racine carrée
 
-x = np.arange(0, 20, 1); #x va de 0 a 20 avec un pas de 1
 
-def u(n): #Syntaxe pour créer une fonction
-    return u(n-1)*0.5+3
 
-y = [] #Creation d'un tableau qui contiendra les valeurs des ordonnés du graphe
-for i in x:
-    y.append(u(i)) #ajoute une case a y valant ce que renvois la fonction u a un rang i
+y = [] #Creation d'un tableau qui contiendra les valeurs des ordonnés
+x = [] #idem pour les abscisses
+
 
 
 def f(x): #fonction qui parle d'elle meme
@@ -17,21 +14,18 @@ def f(x): #fonction qui parle d'elle meme
 
 def rec(i): #fonction necessaire a la creation du graphe
     G = 3 # ordonné commencant a 3 
-    x = [] #creation de tableaux
-    y = []
-    
-    y.append(G)
+    y.append(G)#ajoute une case au tableau qui vaut G
     x.append(0)
-    print(0,G) #premiere ligne d'affichage sur la console.
+    print(0,G)#affiche les valeurs 0 et la valeur de G (ici 3) dans la console
     for n in range(1,i):
         G = f(G)
         y.append(G)
         x.append(n)
-        print(n,G,sqrt(10)-G)
-    return G,x,y
+        print(n,G)#ici des valeurs de n et G
+    return G,x,y #tableaux necessaires par la suite pour faire le graphe.
 
-G,x,y = rec(15)
+G,x,y = rec(10)
 
-plt.plot(x, y, 'r-')
-plt.show()
+plt.plot(x, y, 'r-')#configuration du graphe.
+plt.show() #puis affichage.
 
